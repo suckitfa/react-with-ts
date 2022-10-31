be3fee6242de86fe0080c55a59bdcef901866517
Bob Tang <254202042@qq.com>
2022-10-31 18:28:57 +0800

    react with ts init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Hello from './components/Hello'
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Hello />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/Hello.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+interface IHelloProps {
+    message?:string;
+}
+const Hello:React.FunctionComponent<IHelloProps> = (props:IHelloProps) => {
+    return (
+        <h2>{ props.message}</h2>
+    )
+}
+Hello.defaultProps = {
+    message:"Hello World"
+}
+export default Hello;
