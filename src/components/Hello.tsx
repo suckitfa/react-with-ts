ebad73649969d91714f74786faf716186eb97381
Bob Tang <254202042@qq.com>
2022-11-01 18:15:03 +0800

    interface约束组件

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 interface IHelloProps {
     message?:string;
 }
-const Hello:React.FunctionComponent<IHelloProps> = (props:IHelloProps) => {
+// const Hello:React.FunctionComponent<IHelloProps> = (props:IHelloProps) => {
+//     return (
+//         <h2>{ props.message}</h2>
+//     )
+// }
+const Hello:React.FC<IHelloProps> = (props:IHelloProps) => {
     return (
-        <h2>{ props.message}</h2>
+        <h1>{ props.message}</h1>
     )
 }
 Hello.defaultProps = {
