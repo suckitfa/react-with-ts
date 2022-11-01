fcd07711b2f65e8d2dd440d51a272d11995dc6b6
Bob Tang <254202042@qq.com>
2022-11-01 18:36:43 +0800

    useEffect

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,9 +1,13 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 const LikeButton:React.FC = () => {
     // const [likeObj,setLikeObj] = useState({like:0,on:true})
     const [like,setLike] = useState(0)
     const [on,setOn] = useState(true)
     const message = on === true ? 'ON' : "OFF"
+    // 默认：第一次渲染和每次渲染之后都执行
+    useEffect(() => {
+        window.document.title = `点击了:${like}次`
+    })
     return (
         <>
             <button onClick={() => {setLike(like+1)}}>{like}👍🏻</button>
