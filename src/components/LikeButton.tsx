a7ed356bc78d562bfcd77ceafc9bc58a9ec399e1
Bob Tang <254202042@qq.com>
2022-11-02 20:09:58 +0800

    在App和LikeButton中使用自定义hooks

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,8 +1,10 @@
 import React,{useState,useEffect} from "react";
+import useMousePositionHook from "../hooks/useMousePosition";
 const LikeButton:React.FC = () => {
     // const [likeObj,setLikeObj] = useState({like:0,on:true})
     const [like,setLike] = useState(0)
     const [on,setOn] = useState(true)
+    const {x,y} = useMousePositionHook()
     const message = on === true ? 'ON' : "OFF"
     // 默认：第一次渲染和每次渲染之后都执行
     useEffect(() => {
@@ -11,6 +13,7 @@ const LikeButton:React.FC = () => {
     },[like])
     return (
         <>
+            <h2>x:{x},y:{y}</h2>
             <button onClick={() => {setLike(like+1)}}>{like}👍🏻</button>
             <button onClick={() => {setOn(!on)}}>{message}</button>
         </>
