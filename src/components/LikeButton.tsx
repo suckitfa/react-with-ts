a37a87157ef2cb088c1458ad8d2288e21eab6314
Bob Tang <254202042@qq.com>
2022-11-03 21:29:00 +0800

    通过useRef来访问DOM以及多次渲染之间的纽带

--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,9 +1,12 @@
-import React,{useState,useEffect} from "react";
+import React,{useState,useEffect, useRef} from "react";
 import useMousePositionHook from "../hooks/useMousePosition";
 const LikeButton:React.FC = () => {
     // const [likeObj,setLikeObj] = useState({like:0,on:true})
     const [like,setLike] = useState(0)
     const [on,setOn] = useState(true)
+    const likeRef = useRef(0)
+    const didMountRef = useRef(false)
+    const inputRef = useRef<HTMLInputElement>(null)
     const {x,y} = useMousePositionHook()
     const message = on === true ? 'ON' : "OFF"
     // 默认：第一次渲染和每次渲染之后都执行
@@ -12,11 +15,28 @@ const LikeButton:React.FC = () => {
         window.document.title = `点击了:${like}次`
     },[like])
 
+    useEffect(() => {
+        if (didMountRef.current) {
+            console.log('this is updated')
+        } else {
+            didMountRef.current = true;
+        }
+    })
+
+    const handleFocus = () => {
+        inputRef.current?.focus()
+    }
     // 测试看能够获得最新的state值
     // 不能够获得最新的值
+    // const handleAlertClick = () => {
+    //     setTimeout(() => {
+    //         alert('You clicked on ' + like)
+    //     },3000)
+    // }
+    // 能够获取到最新的值，修改ref的值，不会引发组件的值
     const handleAlertClick = () => {
         setTimeout(() => {
-            alert('You clicked on ' + like)
+            alert('You clicked on ' + likeRef.current)
         },3000)
     }
     // function handleAlertClick() {
@@ -27,7 +47,9 @@ const LikeButton:React.FC = () => {
     return (
         <>
             <h2>x:{x},y:{y}</h2>
-            <button onClick={() => {setLike(like+1)}}>{like}👍🏻</button>
+            <input type="text" ref={inputRef} />
+            <button onClick={handleFocus}>Focus Input!</button>
+            <button onClick={() => {setLike(like+1);likeRef.current++;}}>{like}👍🏻</button>
             <button onClick={handleAlertClick}>Alert</button>
         </>
     )
